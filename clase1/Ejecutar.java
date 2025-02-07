c02126e284baa2627de2595e84b60698936796d0
RobinGuzman76 <robin.guzman@campusucc.edu.co>
2025-02-07 01:20:33 +0000

    Revisar en la casa

--- clase1/Ejecutar.java
+++ clase1/Ejecutar.java
@@ -2,4 +2,5 @@ public class Ejecutar {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!! JAVA");
     }
-}
+    
+//Mirar la sintaxis de java para la proxima clase//
