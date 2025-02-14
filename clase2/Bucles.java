cbf581159946b1c06b976d9ad8eeeb4df750bfe1
RobinGuzman76 <robin.guzman@campusucc.edu.co>
2025-02-14 01:22:30 +0000

    Ejercicios Clase 2

---
+++ clase2/Bucles.java
@@ -0,0 +1,21 @@
+public class Bucles {
+    public static void main (String[] args){
+        //for (inicio; condiciones de parada; incremento o decremento)
+        for (int i = 1; i <= 5; i++){
+            System.out.println("Numero de Iteracion: " + i);
+        }
+        /* Estructura del while
+        *
+        * inicio:
+        * while(condicion){
+        * codigo----------
+        * Incremento o decremento
+        * }
+        */
+        int j = 1;
+        while(j <= 5){
+            j++;
+            System.out.println("Numero de Iteracion: " + j);
+        }
+    }
+}
