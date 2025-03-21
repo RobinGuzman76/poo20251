db5f3ddee58de86c4e41fa8fb812018ad4bd7c20
RobinGuzman76 <robin.guzman@campusucc.edu.co>
2025-03-21 01:09:09 +0000

    Final Herencia

--- clase7/EjecutarHerencia.java
+++ clase7/EjecutarHerencia.java
@@ -9,6 +9,7 @@ public class EjecutarHerencia {
         TB[0] = new Operario(1, "Juan", "Perez", 135.0);
         TB[1] = new Consultor(2, "Maria", "Gomez", 10);
         TB[2] = new Consultor(3, "Pedro", "Garcia", 20);
+        
 
         // Mostrar información
         System.out.println("Trabajadores");
